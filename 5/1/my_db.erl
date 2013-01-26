1b9921b64490066052cb6e9f20dbde9f83226e96
kirill <mokevnin@gmail.com>
2013-01-26 14:03:17 +0400

    add tests

--- 5/1/my_db.erl
+++ 5/1/my_db.erl
@@ -1,6 +1,7 @@
 -module(my_db).
 
--export([start/0, stop/0]).
+%% API
+-export([start/0, stop/0, write/2, read/1, delete/1]).
 
 -export([loop/1]).
 
@@ -13,31 +14,55 @@ stop() ->
   my_db_loop ! stop,
   ok.
 
+write(Key, Value) ->
+  my_db_loop ! {write, Key, Value},
+  ok.
+
+delete(Key) ->
+  my_db_loop ! {delete, Key},
+  ok.
+
+read(Key) ->
+  my_db_loop ! {read, self(), Key},
+  receive
+    Reply -> Reply
+  end.
+
 loop(Db) -> 
   receive
     stop -> 
-      true
+      true;
+    {write, Key, Value} ->
+      Db2 = write(Key, Value, Db),
+      loop(Db2);
+    {read, Pid,  Key} ->
+      Reply = read(Key, Db),
+      Pid ! Reply,
+      loop(Db);
+    {delete, Key} ->
+      Db2 = delete(Key, Db),
+      loop(Db2)
   end.
 
-%write(Key, Value, Db) ->
-  %write(Key, Value, Db, []).
+write(Key, Value, Db) ->
+  write(Key, Value, Db, []).
 
-%write(Key, Value, [], ResultDb) ->
-  %[{Key, Value} | ResultDb];
-%write(Key, Value, [{Key, _} | Tail], ResultDb) ->
-  %[{Key, Value} | ResultDb] ++ Tail;
-%write(Key, Value, [Head | Tail], ResultDb) ->
-  %write(Key, Value, Tail, [Head | ResultDb]).
+write(Key, Value, [], ResultDb) ->
+  [{Key, Value} | ResultDb];
+write(Key, Value, [{Key, _} | Tail], ResultDb) ->
+  [{Key, Value} | ResultDb] ++ Tail;
+write(Key, Value, [Head | Tail], ResultDb) ->
+  write(Key, Value, Tail, [Head | ResultDb]).
 
-%delete(KeyForRemove, Db) ->
-  %[{Key, Value} || {Key, Value} <- Db, KeyForRemove /= Key].
+delete(KeyForRemove, Db) ->
+  [{Key, Value} || {Key, Value} <- Db, KeyForRemove /= Key].
 
-%read(_, []) ->
-  %{error, instance};
-%read(Key, [{Key, Value} | _]) ->
-  %Value;
-%read(Key, [_ | Tail]) -> 
-  %read(Key, Tail).
+read(_, []) ->
+  {error, instance};
+read(Key, [{Key, Value} | _]) ->
+  {ok, Value};
+read(Key, [_ | Tail]) -> 
+  read(Key, Tail).
 
 %match(Value, Db) ->
   %[Key || {Key, Current} <- Db, Current == Value].
--- 5/1/my_db_tests.erl
+++ 5/1/my_db_tests.erl
@@ -5,7 +5,9 @@ start_stop_test_() ->
   {setup,
     fun start/0,
     fun stop/1,
-    [fun stop_success/0]}.
+    [fun write_read_success/0,
+     fun read_error/0,
+     fun delete_success/0]}.
 
 
 start() ->
@@ -14,48 +16,16 @@ start() ->
 stop(_) -> 
   my_db:stop().
 
-stop_success() -> 
-  ?assertEqual(ok, my_db:stop()).
+write_read_success() -> 
+  ?assertEqual(ok, my_db:write(key, el)),
+  ?assertEqual({ok, el}, my_db:read(key)).
 
-%new_test() ->
-  %?assertEqual([], db:new()).
+read_error() ->
+  ?assertEqual({error, instance}, my_db:read(undefined_key)).
 
-%destroy_test() ->
-  %Db = db:new(),
-  %?assertEqual(ok, db:destroy(Db)).
+delete_success() ->
+  ?assertEqual(ok, my_db:write(key, el)),
+  ?assertEqual(ok, my_db:delete(key)),
+  ?assertEqual({error, instance}, my_db:read(key)).
 
-%write_test() ->
-  %Db = db:new(),
-  %Db2 = db:write(key1, el1, Db),
-  %?assertEqual([{key1, el1}], Db2),
 
-  %Db3 = db:write(key2, el2, Db2),
-  %?assertEqual([{key2, el2}, {key1, el1}], Db3),
-
-  %Db4 = db:write(key1, el3, Db3),
-  %?assertEqual([{key1, el3}, {key2, el2}], Db4).
-
-%read_test() ->
-  %Db = db:new(),
-  %Db2 = db:write(key1, el1, Db),
-  %Db3 = db:write(key2, el2, Db2),
-
-  %?assertEqual(el1, db:read(key1, Db3)),
-  %?assertEqual(el2, db:read(key2, Db3)),
-  %?assertEqual({error, instance}, db:read(unknown_key, Db3)).
-
-%delete_test() ->
-  %Db = db:new(),
-  %Db2 = db:write(key1, el1, Db),
-  %Db3 = db:write(key2, el2, Db2),
-  
-  %Db4 = db:delete(key1, Db3),
-  %?assertEqual([{key2, el2}], Db4).
-
-%match_test() ->
-  %Db = db:new(),
-  %Db2 = db:write(key1, el1, Db),
-  %Db3 = db:write(key2, el2, Db2),
-  %Db4 = db:write(key3, el2, Db3),
-
-  %?assertEqual([key3, key2], db:match(el2, Db4)).
